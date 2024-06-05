d18a1873aec1930592b6f84ac547f54fe12d0cc0
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-06-05 07:36:31 +0530

    Add files via upload

---
+++ Lecture 3/Lecture3.cpp
@@ -0,0 +1,18 @@
+/*Data types-It defines the type of varaible declared
+Must Begin with only alphabet
+Cpp is case sensitive
+There are different types data types-
+(Integer,Float,String,Double,Bool,void,Struct,Enum)
+Declaration of variable is (DataType name of variable)*/
+
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+
+
+}
+
