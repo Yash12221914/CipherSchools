fbfccdff59d474626b6bd69bb7bcfa3237529428
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-06-05 07:46:34 +0530

    Add files via upload

---
+++ Lecture 4/Lecture 4.cpp
@@ -0,0 +1,18 @@
+//Task - Declare 2 variables and then assign them value of 3,5 and then add them
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int a;  //Declaring two variable
+    int b;
+
+    a = 3;  //Assigning the variable with values
+    b = 5;
+
+    int c;
+    c = a+b;  //Adding the values stored in variable
+    cout<<c<<endl;  //Showing the added values
+    return 0;
+}
