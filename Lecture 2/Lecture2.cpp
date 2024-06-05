dbaee0c70540b6180582e3acb29584449ef40263
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-06-05 07:23:03 +0530

    Add files via upload

---
+++ Lecture 2/Lecture2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>   //This is Header
+
+using namespace std;
+
+int main(){
+
+    cout<<"Hello World"<<endl;   //This will print Hello World
+
+}
+
+//This is single line comment
+
+
+/*  This is
+    multi line
+    comment
+*/
