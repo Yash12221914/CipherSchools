7e0a0e034c8cbcd343713d7172282bd56ad8bba3
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-06-06 22:30:32 +0530

    Add files via upload

---
+++ Lecture 7/Lecture7.cpp
@@ -0,0 +1,12 @@
+//Array - It is used to store multiple data of same data type in a single variable
+
+//It can be initialised as - (datatype nameOfTheVariable[sizeOfTheArray]).
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[5];//it can store value of Integer data type
+    //it can store 6 element as cpp is allowing 0 based indexing.
+    return 0;
+}
