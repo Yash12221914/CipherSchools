9b09472aef70630dad3ee8de5e8cdd4107783ba9
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-06-08 22:43:54 +0530

    Add files via upload

---
+++ Lecture 9/Lecture9.cpp
@@ -0,0 +1,16 @@
+//Loops in which similar action is reapeted.
+//action shold be decide in every loop
+
+
+//1.For Loop and 2.While Loop
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    for(int i = 0;i<5;i++){
+        cout<<i<<" "<<endl;
+    }
+
+    return 0;
+}
