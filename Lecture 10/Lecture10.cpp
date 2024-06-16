744f7043cc60c3f10d4d1b9f542063e776557bce
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-06-16 11:21:27 +0530

    Add files via upload

---
+++ Lecture 10/Lecture10.cpp
@@ -0,0 +1,25 @@
+//For loop with post increment and pre increment
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    // int i = 0;
+    // int j = 0;
+    // int k = 0;
+
+    // cout<<i<<j<<k<<endl;;
+    // j = i++;
+    // cout<<i<<j<<k<<endl;
+    // k = i++;
+    // cout<<i<<j<<k<<endl;
+
+    //printing numbers from 1 to 100 using conditions
+
+    for(int i = 1;i<=100;i++){
+        cout<<i<<endl;
+    }
+    return 0;
+}
