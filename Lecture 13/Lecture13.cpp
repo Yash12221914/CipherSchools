85f916302d2d4f24e148783ed52d1ebeb18f1fa0
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-06-20 00:17:06 +0530

    Add files via upload

---
+++ Lecture 13/Lecture13.cpp
@@ -0,0 +1,16 @@
+//functions
+// uses
+
+//Uses divide & conqoure
+
+//Reuse
+
+//reduces less Line of code
+
+//Repeataion of code 
+
+//abstraction
+
+//Performs specific task and can be called multiple times
+
+//it consist of two things 1. Header and 2. body..
