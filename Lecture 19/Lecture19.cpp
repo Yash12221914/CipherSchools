2c385dcfee4fe6d87e4b3510289f3e1056863e8b
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-06-21 13:46:23 +0530

    Add files via upload

---
+++ Lecture 19/Lecture19.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+//introduction of address 
+
+int main(){
+    int a;
+    long long int b;
+    cout<<"The size of int : "<<sizeof(a)<<" the size of long long int: "<<sizeof(b)<<endl;
+    return 0;
+}
