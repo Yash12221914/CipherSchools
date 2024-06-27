a760a7611e9c691a69b9d22486ae2635a7a95e31
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-06-27 15:45:55 +0530

    Add files via upload

---
+++ Lecture 27/Lecture27.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int a;
+    // int b;
+    // cin>>a;
+    // cin>>b;
+    // int f = a+b;
+    // string c;
+    // string d;
+    // cin>>c;
+    // string e = c+d;
+    // cout<<f<<endl;//this is called addition
+    // cout<<e<<endl;//this is called string concatination
+    //printing each character of string
+    // for(int i = 0;i<5;i++){
+    //     cout<<c[i]<<endl;
+    // }
+    // //to get full line as an input
+    string g;
+    getline(cin,g);
+    cout<<g<<endl;
+}
