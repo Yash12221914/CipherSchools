e9251ad4c899dee3cd063a0a3eaeff3b146a5bc8
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-06-27 15:22:52 +0530

    Add files via upload

---
+++ Lecture 26/Lecture26.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // char y,a,s,h;
+    // cin>>y>>a>>s>>h;
+
+    // cout<<"The name is : "<<y<<a<<s<<h;
+
+    // char name[4];
+
+    // cout<<"Enter the name: "<<endl;
+    // for(int i = 0;i<4;i++){
+    //     cin>>name[i];
+    // }
+
+    // cout<<"The name is: "<<endl;
+    // for(int i = 0;i<4;i++){
+    //     cout<<name[i]; 
+    // }
+
+    //if we do not know the length
+
+    // char name[1000];
+    // cout<<"Enter the name"<<endl;
+
+    // int i = 0;
+    // while(1){
+    //     cin>>name[i];
+    //     if(name[i] == '#'){
+    //         break;
+    //     }
+    //     i++;
+    // }
+
+    // cout<<"The name is: "<<endl;
+    // i = 0;
+    // while(name[i] != '#'){
+    //     cout<<name[i];
+    //     i++;
+    // }
+
+    //string can reduce the restriction thee size of character that we want to store
+    string name;
+    cin>>name;
+    cout<<name;
+
+    return 0;
+}
