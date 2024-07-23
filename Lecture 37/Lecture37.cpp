90eda9339882fb469c7f691a1e2f0aa7d1cc4b0b
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-07-24 00:02:53 +0530

    Add files via upload

---
+++ Lecture 37/Lecture37.cpp
@@ -0,0 +1,7 @@
+//Selection sort
+#include<iostream>
+using namespace std;
+
+int main(){
+    //it is mainly we need to find the least element and then swap it to 1st indexed element and do this process for every itteration.
+}
