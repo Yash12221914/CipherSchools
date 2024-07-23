8433c23b4cac99f1c9c3eaf7e407f11330edf0e6
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-07-23 23:01:32 +0530

    Add files via upload

---
+++ Lecture 36/Lecture36.cpp
@@ -0,0 +1,9 @@
+//Algorithms
+
+//Algorithms are the procedure which are having different approaches to calculate 
+//or solve any problem in a better or efficient way
+
+//Correct use of algorithm leads to less time complexity as the problems get complexed the time also increses so we need better alogorithm
+
+//there are different types of algorithm like
+//1.searching,sorting, recursion, dp,gready and many more
