4502bbed7777f934ebbba1c9a2f7d93a5f13efa0
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-07-26 10:46:06 +0530

    Add files via upload

---
+++ Lecture 42/Lecture42.cpp
@@ -0,0 +1,10 @@
+//The number of times each line of the pseudo code runs maters
+//implementation of logic remains the same
+
+//for insertion sort
+//compiler checks every iteration that j is greater than n or not
+//all line takes c1n+c2(n-1)+c3(n-1) times
+
+//There is concept of best case worst case and average case
+//best case - it happens when there is minnum mumber of compiler checks for a solution and takes minimum time
+//worst case - It happens  when there is maximum number of compiler checks for a solution and takes maximum time
