74b13697276679232bf254032c19a25f9a899de0
Yash Raj <119494588+Yash12221914@users.noreply.github.com>
2024-07-26 22:41:05 +0530

    Add files via upload

---
+++ Lecture 46/Lecture46.cpp
@@ -0,0 +1,8 @@
+//Gready algorithms
+//It is a way of approaching for a problem to get a solution in a greedy way
+//In other words, Focusing on a particular instant to find a solution independent of what happen after
+
+//Greedy algorithm repeatedly makes locally choice/decision
+//They are often intutive, easy to understand and easy to implement
+//However the problem is that in many situations we cannot solve a problem using a greedy approach
+//Greedy solution is not necessarily best
